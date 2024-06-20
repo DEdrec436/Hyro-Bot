24993d2cd4baa70a2e2add41920590dfe8e4288b
DEdrec436 <111685272+DEdrec436@users.noreply.github.com>
2024-06-20 17:01:36 +0500

    Hyro bot.py

--- bot.py
+++ bot.py
@@ -20,8 +20,23 @@ def send_welcome(message):
 """)
 @bot.message_handler(commands=['coin'])
 def coin_handler(message):
-    coin = choice(["ОРЕЛ", "РЕШКА"])
+    coin = choice([
+        
+        "ОРЕЛ-источник: https://platform.kodland.org/ru/task_96343/", 
+        "РЕШКА-источник: https://platform.kodland.org/ru/task_96343/" ])
     bot.reply_to(message, coin)
+@bot.message_handler(commands=['fact'])
+def fact_handler(message):
+        fact = choice(["Интересный факт - Batman: Arkham Asylum чуть не стала ритм-игрой.На ранних уровнях разработки боевая система в прототипе Batman: Arkham Asylum выглядела как ритм-игра. Второй прототип проводил встречи с врагами в виде двумерного файтинга. Обе эти попытки были отбракованы, но их наработки воплотились в итоговом варианте игры.- https://medium.com/cybervalhalla/",
+                        "Интересный факт - Первая видеоигра в космосе.Тетрис. В 1993 году Game Boy с игрой оказался на борту ракеты “Союз ТМ-17”, а затем — на космической станции “Мир”, где в него играл космонавт Александр Серебров. Позже игру продали на аукционе за $1220. - https://medium.com/cybervalhalla/ "])
+        bot.reply_to(message, fact)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text) 
+
+
+    
+
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
